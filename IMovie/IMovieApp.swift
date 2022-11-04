1bff2552bb807c61357c8447e1be6adb6432b67c
Iury Vasconcelos <iury0393@gmail.com>
2022-11-04 14:36:48 -0300

    Initial Commit

---
+++ IMovie/IMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMovieApp.swift
+//  IMovie
+//
+//  Created by Iury Vasconcelos on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct IMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
